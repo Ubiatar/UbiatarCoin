559472a96f9fd8b8ec637f2181f59d7f5b40595e
antoniogiordano <antonio.giordano@getapper.com>
2018-02-12 15:18:34 +0100

    Reservation contract improved

--- contracts/ReservationContract.sol
+++ contracts/ReservationContract.sol
@@ -5,14 +5,9 @@ pragma solidity ^0.4.18;
 import "./SafeMath.sol";
 import "./Owned.sol";
 
-contract UAC
-{
-
-}
-
 contract ICO
 {
-
+    // @TODO: put interface
 }
 
 contract ReservationContract is Owned
@@ -24,7 +19,6 @@ contract ReservationContract is Owned
     uint public icoBlockNumberStart = 0;
     uint public rcBlockNumberStart = 0;
     mapping(address => uint) investorsTokens;
-    UAC public uacToken;
     ICO public ico;
 
     function ReservationContract() {}
@@ -42,6 +36,12 @@ contract ReservationContract is Owned
         _;
     }
 
+    modifier onlyAfterIcoStarted()
+    {
+        require(block.number > icoBlockNumberStart);
+        _;
+    }
+
     function setIcoContractAddress(address _icoContractAddress)
     onlyOwner
     {
@@ -53,66 +53,39 @@ contract ReservationContract is Owned
     onlyOwner
     {
         uacContractAddress = _uacContractAddress;
-        uacToken = UAC(_uacTokenAddress);
     }
 
     function getIcoBlockNumberStart()
     onlyOwner
     {
-        // @TODO: implement getBlockNumberStart ICO function
         icoBlockNumberStart = ico.getBlockNumberStart();
         rcBlockNumberStart = icoBlockNumberStart - uint(2 days / 17 seconds);
     }
 
-    function() payable
+    function()
+    payable
     onlyInBlockNumberRange
     {
-        require(msg.value > 500 finney);
-        // @TODO: call ICO to reserve tokens
-        // @TODO: calculate tokens to give to investor
+        require(msg.value >= 500 finney);
+        uint tokens = ico.reserveTokensRC();
+        investorsTokens[msg.sender].add(tokens);
     }
 
-    function getReservedTokens(address user)
+    function getReservedTokens(address investor)
     constant
     public
     returns (uint balance)
     {
-        return investorsTokens(msg.sender);
+        return investorsTokens(investor);
     }
 
     function withdrawTokens()
     public
+    onlyAfterIcoStarted
     {
-        uint tempBalance = (investors[msg.sender].initialBalance.mul(1 ether)).div(3);
-        amountToSend = 0;
-
-
-        if ((uint(now) >= firstThreshold) && (investors[msg.sender].firstWithdraw == 0)) {
-            investors[msg.sender].balance = investors[msg.sender].initialBalance;
-            investors[msg.sender].lastWithdrawTime = secondThreshold;
-            investors[msg.sender].firstWithdraw = 1;
-            amountToSend = tempBalance;
-        }
-
-        tempBalance = tempBalance.mul(2);
-
-        if (uint(now) >= secondThreshold) {
-            uint daysPassed = (uint(now).sub(investors[msg.sender].lastWithdrawTime)).div(1 days);
-            amountToSend = amountToSend.add((tempBalance.div(180)).mul(daysPassed));
-            investors[msg.sender].lastWithdrawTime = uint(now);
-        }
-
-        require(amountToSend != 0);
-        amountToSend = amountToSend.div(1 ether);
-
-        if (investors[msg.sender].balance < amountToSend) {
-            amountToSend = investors[msg.sender].balance;
-        }
-
-        investors[msg.sender].balance = investors[msg.sender].balance.sub(amountToSend);
-
-        uacToken.transfer(msg.sender, amountToSend);
-
-        amountToSend = 0;
+        uint tokenToBeIssued = investorsTokens[msg.sender];
+        require(tokenToBeIssued > 0);
+        investorsTokens[msg.sender] = 0;
+        require(ico.reclaimTokensRC(msg.sender, tokenToBeIssued));
     }
 }
