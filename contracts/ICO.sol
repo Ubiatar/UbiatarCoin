4f9e3356ca61c1d7533882bf7421491bea78a4b9
antoniogiordano <antonio.giordano@getapper.com>
2018-03-03 14:12:55 +0100

    Contracts commented and minor fixes

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.4.18;
 import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
 import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
 
+/*
+    Eidoo ICO Engine interface
+    This interface enables Eidoo wallet to query our ICO and display all the informations needed in the app
+*/
 contract ICOEngineInterface{
     function started() public view returns(bool);
     function ended() public view returns(bool);
@@ -15,6 +19,7 @@ contract ICOEngineInterface{
     function price() public view returns(uint);
 }
 
+// UbiatarCoin Abstract Contract
 contract UACAC {
     function lockTransfer(bool _lock) public;
 
@@ -23,54 +28,63 @@ contract UACAC {
     function balanceOf(address _owner) public constant returns (uint256);
 }
 
+// PreSaleVesting Abstract Contract
 contract PreSaleVestingAC {
     function finishIco();
 }
 
+// Founders Vesting Abstract Contract
 contract FoundersVestingAC {
     function finishIco() public;
 }
 
+// UbiatarPlay Abstract Contract
 contract UbiatarPlayAC {
     function finishIco();
 }
 
-// This is the main UbiatarCoin ICO smart contract
+/*
+    ICO crowdsale main contract
+    It is in charge to issue all the token for the ICO, preSales, advisors and founders vesting
+*/
 contract ICO is Ownable, ICOEngineInterface {
 
+    // SafeMath standard lib
     using SafeMath for uint;
 
+    // total Wei collected during the ICO
     uint collectedWei = 0;
 
-    // Standard token price is 200 dollar CENTS per token
+    // Standard token price in USD CENTS per token
     uint public usdTokenPrice = 2 * 100;
 
     // The USD/ETH
     // UPDATE CHANGE RATE WITH CURRENT RATE WHEN DEPLOYING
-    // This value is given in dollar CENTS
+    // This value is given in USD CENTS
     uint public usdPerEth = 1100 * 100;
 
     // Founders reward
     uint public constant FOUNDERS_REWARD = 12000000 * 1 ether;
-    // Tokens bought in PreSale
+    // Total Tokens bought in PreSale
     uint public constant PRESALE_REWARD = 17584778551358900100698693;
-    // 15 000 000 tokens sold during the ICO
+    // 15 000 000 tokens on sale during the ICO
     uint public constant ICO_TOKEN_SUPPLY_LIMIT = 15000000 * 1 ether;
     // Tokens for advisors
     uint public constant ADVISORS_TOKENS = 4915221448641099899301307;
     // 50 500 000 tokens for Ubiatar Play
     uint public constant UBIATARPLAY_TOKENS = 50500000 * 1 ether;
 
-    // Fields:
+    /// Fields:
 
-    // Test block number from which Ico will start, to be updated with real value before ICO starts
+    // ICO starting block number
     uint public icoBlockNumberStart = 5305785;
 
-    // Test time in epoch time when Ico will finish, to be updated with real value before ICO starts
+    // ICO finish time in epoch time
     uint public icoFinishTime = 1524488400;
 
+    // ICO partecipant to be refund in case of overflow on the last token purchase
     address public toBeRefund = 0x0;
-
+    // ICO refund amount in case of overflow on the last token purchase
     uint public refundAmount;
 
     // Total amount of tokens sold during ICO
@@ -83,69 +97,75 @@ contract ICO is Ownable, ICOEngineInterface {
     address public preSaleVestingAddress = 0x0;
     // This is where UBIATARPLAY_TOKENS will be allocated
     address public ubiatarPlayAddress = 0x0;
-
+    // UbiatarCoin ERC20 address
     address public uacTokenAddress = 0x0;
-
+    // Unsold token contract address
     address public unsoldContractAddress = 0x0;
-
+    // This is where ADVISORS_TOKEN will be allocated
     address public advisorsWalletAddress = 0x0;
-
+    // This is where Ethers will be transfered
     address public ubiatarColdWallet = 0x0;
 
+    // UbiatarCoin contract reference
     UACAC public uacToken;
-
+    // PreSaleVesting contract reference
     PreSaleVestingAC public preSaleVesting;
-
+    // FoundersVesting contract reference
     FoundersVestingAC public foundersVesting;
-
+    // UbiatarPlay contract reference
     UbiatarPlayAC public ubiatarPlay;
 
+    // ICO possibles state.
     enum State
     {
         Init,
-
         ICORunning,
         ICOPaused,
-
-        // Collected ETH is transferred to multisigs.
-        // Unsold tokens transferred to UACUnsold contract.
         ICOFinished
     }
 
+    // ICO current state, initialized with Init state
     State public currentState = State.Init;
 
-    // Modifiers:
+    /// Modifiers
+
+    // Only in a given ICO state
     modifier onlyInState(State state)
     {
         require(state == currentState);
         _;
     }
 
+    // Only before ICO crowdsale starting block number
     modifier onlyBeforeBlockNumber()
     {
         require(block.number < icoBlockNumberStart);
         _;
     }
 
+    // Only after ICO crowdsale starting block number
     modifier onlyAfterBlockNumber()
     {
         require(block.number >= icoBlockNumberStart);
         _;
     }
 
+    // Only before ICO crowdsale finishing epoch time
     modifier onlyBeforeIcoFinishTime()
     {
         require(uint(now) <= icoFinishTime);
         _;
     }
 
+    // Only if ICO can finish (so after finishing epoch time, or when all tokens are sold)
     modifier canFinishICO()
     {
         require((uint(now) >= icoFinishTime) || (icoTokensSold == ICO_TOKEN_SUPPLY_LIMIT));
         _;
     }
 
-    // Events:
+    /// Events
+
     event LogStateSwitch(State newState);
     event LogBuy(address owner, uint value);
     event LogBurn(address owner, uint value);
@@ -168,8 +188,9 @@ contract ICO is Ownable, ICOEngineInterface {
     event LogResumeICO();
     event LogFinishICO();
 
-    // Functions:
-    /// @dev Constructor
+    ///  Functions:
+
+    // ICO constructor. It takes main contracts adresses
     function ICO
     (
         address _uacTokenAddress,
@@ -193,6 +214,7 @@ contract ICO is Ownable, ICOEngineInterface {
         advisorsWalletAddress = _advisorsWalletAddress;
     }
 
+    // It will put ICO in Running state, it should be launched before starting block
     function startICO()
     public
     onlyOwner
@@ -207,6 +229,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogStartICO();
     }
 
+    // It sets ICO in Pause state
     function pauseICO()
     public
     onlyOwner
@@ -216,6 +239,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogPauseICO();
     }
 
+    // It resotres ICO in Running state
     function resumeICO()
     public
     onlyOwner
@@ -225,9 +249,11 @@ contract ICO is Ownable, ICOEngineInterface {
         LogResumeICO();
     }
 
+    // It allows to withdraw crowdsale Ether only when ICO is finished
     function withdraw(uint withdrawAmount)
     public
     onlyOwner
+    onlyInState(State.ICOFinished)
     {
         require(ubiatarColdWallet != 0x0);
         if(withdrawAmount > this.balance) {
@@ -237,7 +263,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogWithdraw(ubiatarColdWallet, withdrawAmount);
     }
 
-    /// This function can be called by owner only when the ICO is finished
+    // It will set ICO in Finished state and it will call finish functions in side contracts
     function finishICO()
     public
     onlyOwner
@@ -262,6 +288,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogFinishICO();
     }
 
+    // It will refund last address just in case of overflow
     function refund()
     public
     onlyOwner
@@ -276,6 +303,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogRefund(_toBeRefund, _refundAmount);
     }
 
+    // This is the main function for Token purchase during ICO. It takes a buyer address where tokens should be issued
     function buyTokens(address _buyer)
     internal
     onlyInState(State.ICORunning)
@@ -326,6 +354,7 @@ contract ICO is Ownable, ICOEngineInterface {
         }
     }
 
+    // It is an internal function that will call UAC ERC20 contract to issue the tokens
     function issueTokensInternal(address _to, uint _tokens)
     internal
     {
@@ -337,7 +366,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogBuy(_to, _tokens);
     }
 
-    //Setters
+    /// Setters
 
     function setUbiatarColdWallet(address _ubiatarColdWallet)
     public
@@ -405,6 +434,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogPreSaleVestingAddressSet(preSaleVestingAddress);
     }
 
+    // It will set ICO crowdsale starting block number
     function setBlockNumberStart(uint _blockNumber)
     public
     onlyOwner
@@ -413,6 +443,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogBlockNumberStartSet(icoBlockNumberStart);
     }
 
+    // It will set ICO finishing epoch time
     function setIcoFinishTime(uint _time)
     public
     onlyOwner
@@ -429,6 +460,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogUsdPerEthRateSet(usdPerEth);
     }
 
+    // It will switch ICO State
     function setState(State _s)
     internal
     {
@@ -444,7 +476,7 @@ contract ICO is Ownable, ICOEngineInterface {
         LogUsdTokenPrice(usdTokenPrice);
     }
 
-    // Getters
+    /// Getters
 
     function getBlockNumberStart()
     constant
@@ -512,7 +544,7 @@ contract ICO is Ownable, ICOEngineInterface {
         return uacPerEth;
     }
 
-    // ICOEngineInterface
+    /// ICOEngineInterface
 
     // false if the ico is not started, true if the ico is started and running, true if the ico is completed
     function started()
@@ -536,14 +568,7 @@ contract ICO is Ownable, ICOEngineInterface {
     view
     returns(bool)
     {
-        if(currentState == State.ICOFinished)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return ((uint(now) >= icoFinishTime) || (icoTokensSold == ICO_TOKEN_SUPPLY_LIMIT));
     }
 
     // time stamp of the starting time of the ico, must return 0 if it depends on the block number
@@ -629,6 +654,7 @@ contract ICO is Ownable, ICOEngineInterface {
         return getUacTokensPerEth(bonusPercent);
     }
 
+    // It allows to buy tokens for an other address than msg.sender
     function buyTokensFor(address _to)
     payable
     public {
@@ -638,7 +664,6 @@ contract ICO is Ownable, ICOEngineInterface {
 
     // Default fallback function
     function() payable {
-        // buyTokens -> issueTokensInternal
         buyTokens(msg.sender);
     }
 }
