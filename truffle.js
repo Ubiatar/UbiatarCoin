ddcf5ff1e54fa8eb3618c6350ee6f8b829be3901
fabio <fabio.padoin@gmail.com>
2018-03-03 15:06:41 +0100

    Updated package.json and truffle.js

--- truffle.js
+++ truffle.js
@@ -1,25 +1,10 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-
-var infura_apikey = "";
-var mnemonic = "";
-
 module.exports = {
     networks: {
-        ganache: {
-            host: "localhost",
-            port: 8545,
-            network_id: "42",
-            gas: 300000000,
-        },
         development: {
             host: "localhost",
             port: 8545,
             network_id: "*",
             gas: 5000000,
-        },
-        ropsten: {
-            provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey),
-            network_id: 3,
         }
     }
 };
